e1a019de7c01ba7f313e71ebc8e93cdad7d9a9de
Orlov <dvaoru@gmail.com>
2025-11-21 14:40:11 +0300

    Отправка сообщения о сборе яблока

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -1,23 +1,44 @@
 import { Room, Client } from "colyseus";
-import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
+import { log } from "console";
+
+export class Vector2float extends Schema {
+    @type("uint32") id = 0;
+    @type("number") x = Math.floor(Math.random() * 256) - 128;
+    @type("number") z = Math.floor(Math.random() * 256) - 128;
+}
 
 export class Player extends Schema {
     @type("number") x = Math.floor(Math.random() * 256) - 128;
     @type("number") z = Math.floor(Math.random() * 256) - 128;
     @type("uint8") d = 2;
-    @type("uint8") type = 0; //Тип змейки 
-
+    @type("uint8") type = 0; //Тип змейки
 }
 
 export class State extends Schema {
-    @type({ map: Player })
-    players = new MapSchema<Player>();
+    @type({ map: Player }) players = new MapSchema<Player>();
+    @type([Vector2float]) apples = new ArraySchema<Vector2float>();
 
-    something = "This attribute won't be sent to the client-side";
+    appleLastId = 0;
+
+    createApple() {
+        const apple = new Vector2float();
+        apple.id = this.appleLastId;
+        this.appleLastId++;
+        this.apples.push(apple);
+    }
+
+    collectApple(player: Player, data: any) {
+        const apple = this.apples.find((value => value.id === data.id));
+        console.log("apple = " + apple)
+        if (apple === undefined) return;
+        apple.x = Math.floor(Math.random() * 256) - 128;
+        apple.z = Math.floor(Math.random() * 256) - 128;
+    }
 
     createPlayer(sessionId: string, snakeType: number) {
         const player = new Player();
-        player.type = snakeType; 
+        player.type = snakeType;
         this.players.set(sessionId, player);
     }
 
@@ -33,6 +54,7 @@ export class State extends Schema {
 
 export class StateHandlerRoom extends Room<State> {
     maxClients = 4;
+    startAppleCount = 100;
 
     onCreate(options) {
         console.log("StateHandlerRoom created!", options);
@@ -43,10 +65,20 @@ export class StateHandlerRoom extends Room<State> {
             this.state.movePlayer(client.sessionId, data);
         });
 
-        this.onMessage("skin", (client, data) => {
+        this.onMessage("collect", (client, data) => {
+            console.log("Пришло сообщение " + "collect " + data.id +" " + data );
             const player = this.state.players.get(client.sessionId);
-            player.type = data.t;
-});
+            this.state.collectApple(player, data);
+        });
+
+        // this.onMessage("skin", (client, data) => {
+        //     const player = this.state.players.get(client.sessionId);
+        //     player.type = data.t;
+        // });
+
+        for (let i = 0; i < this.startAppleCount; i++) {
+            this.state.createApple();
+        }
     }
 
     onAuth(client, options, req) {
